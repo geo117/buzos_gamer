634b78b24975014c9a76e00d209fb6fb2d7cb903
geo117 <122947701+geo117@users.noreply.github.com>
2023-09-20 15:22:39 -0500

    seegunda subida de codigo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload new roads salida de prueba.
+          Edit <code>src/App.js</code>
         </p>
         <a
           className="App-link"
