45c39956e7f12706be586a05bf226e4cb3146991
geo117 <122947701+geo117@users.noreply.github.com>
2023-09-20 16:56:48 -0500

    subia primaria de codigo

---
+++ src/componentes/principal/inicio.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Inicio = () => {
+
+    return(
+        <div className="container bg-secondary">
+            <div className="py-1">
+                <h1>salida de prueba</h1>
+            </div>
+            
+        </div>
+    )
+};
+
+export default Inicio;
