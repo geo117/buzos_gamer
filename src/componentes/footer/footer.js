e537400cffdf025176c0eaba957302191642b20e
geo117 <122947701+geo117@users.noreply.github.com>
2023-09-22 15:43:17 -0500

    se hacen modificaciones en el footer

--- src/componentes/footer/footer.js
+++ src/componentes/footer/footer.js
@@ -4,18 +4,20 @@ const Fotter = () => {
 
     return (
         <div>
-            <div className="footer1 bg-success">
-                <div className="container-fluid text-center">
-                    <div className="row">
-                        <div className="col-md-4">
-                            Column
-                        </div>
-                        <div className="col-md-8">
-                            Column
+            <footer>
+                <div className="footer1 bg-success">
+                    <div className="container-fluid text-center">
+                        <div className="row">
+                            <div className="col-md-4">
+                                Column
+                            </div>
+                            <div className="col-md-8">
+                                Column
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </footer>
         </div>
     )
 }
--- src/componentes/infoImg/InfoImg.js
+++ src/componentes/infoImg/InfoImg.js
@@ -6,9 +6,12 @@ const InfoImg = () => {
     return(
         <div className="container-fluid">
             <div className="my-2 p-1 bg-info">
-                <p className="m-0">
-                    <b>salida de prueba</b>
+                <p className="m-0 fs-3">
+                    <b>Informacion del producto</b>
                 </p>
+            </div>
+            <div className="bg-white">
+                <p>salida de prueba</p>
             </div>
         </div>
     );
